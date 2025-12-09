292dce5f451002086bde9a0e248403943d220a2b
Klimpavlov <Klimpavlov2002@gmail.com>
2025-12-09 10:33:14 +0300

    trpc setup

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -2,13 +2,16 @@ import {HomeView} from "@/modules/home/ui/view/home-view";
 import {auth} from "@/lib/auth";
 import {headers} from "next/headers";
 import {redirect} from "next/navigation";
+import {caller} from "@/trpc/server";
 
 export default async function Page() {
   const session = await auth.api.getSession({
     headers: await headers()
   })
+
   if(!session) {
     redirect("/sign-in")
   }
+
   return <HomeView/>
 }
